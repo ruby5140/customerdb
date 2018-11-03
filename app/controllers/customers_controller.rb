193323568900f92940fa802cf4753f2e5d656b24
e.hanyu <e.hanyu@freebit.net>
2018-11-03 08:24:58 +0000

    edit

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -5,21 +5,42 @@ class CustomersController < ApplicationController
     end
     
     def show
+        @customers = Customer.find(params[:id])
     end
     
     def new
+        @customers = Customer.new
     end
     
     def create
+        @customers = Customer.new(customer_params)
+        
+        if @customer.save
+          flash[:success] = "正常に登録されました"
+          redirect_to @customer
+        else
+          flash.now[:danger] = "登録に失敗しました"  
+          render :new
+        end
+        
     end
     
     def edit
+        @customers = Customer.find(params[:id])
     end
     
     def update
     end
     
     def destroy
+    end
+    
+    private
+    
+    # Strong Parameter
+    def customer_params
+        params.require(:customer).permit(:company , :phone , :server , :remark)
+    
     end
     
 end
