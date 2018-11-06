959d5b59f3bc1d701da5834e40e9ab4d33088ce8
e.hanyu <e.hanyu@freebit.net>
2018-11-06 08:30:41 +0000

    validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,11 @@
 class Customer < ApplicationRecord
   
-  
+  # バリデーション 
+  validates :company, presence: true, length: { maximum: 255 }
+  validates :phone, presence: true, length: { maximum: 255 }
+  validates :server, presence: true, length: { maximum: 255 }
+  validates :remark, presence: true, length: { maximum: 255 }
+
   def self.search(search)
     if search 
      Customer.where(['company LIKE ?', "%#{search}%"])
