5f2dbd4a38b05d8b2e25c21637a636d98916174c
e.hanyu <e.hanyu@freebit.net>
2018-11-18 04:15:24 +0000

    top

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #get 'top/index'
+  # topページ追加
+  root to: 'top#index'
 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # RESTful + search + csvを追加
   resources :customers do
         collection do
